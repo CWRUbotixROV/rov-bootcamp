b3061f8f952d0329eff03904b503a52c2233b895
CoolioYo <georgiacmartinez@gmail.com>
2021-09-12 22:20:26 -0400

    Created get_contours function

--- shape_classifier.py
+++ shape_classifier.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 
 def otsu(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -7,4 +6,23 @@ def otsu(image):
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
     ret, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
+    # Inverting image (switching black and white)
+    # thresh = (255-thresh)
+
     return thresh
+
+
+def get_contours(image, thresh):
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Get image dimensions to calculate max_area
+    height, width, channels = image.shape
+    max_area = height * width * .8
+
+    # Draw rectangle around central contour
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+
+        if 1000 < area < max_area:
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
--- test.py
+++ test.py
@@ -4,10 +4,11 @@ import utils
 
 images, shape_label = utils.get_all_training_data()
 
-image = cv2.imread(images[3])
+image = cv2.imread(images[100])
 
 otsu_image = shape_classifier.otsu(image)
-cv2.imshow("image", otsu_image)
+shape_classifier.get_contours(image, otsu_image)
 
+cv2.imshow("image", image)
 cv2.waitKey(0)
 
